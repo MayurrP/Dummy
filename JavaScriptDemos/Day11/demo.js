0fed8d80c3f83a78283c1d99f8b19f1b8e6b009f
mayur <mayurpakhale30.com>
2023-01-13 11:18:42 +0530

    fe

---
+++ JavaScriptDemos/Day11/demo.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+            $("#mydiv1").css({"background-color":"yellow",
+            "color":"blue",
+            "font-face":"Bold",
+            "font-size":"40px"
+            })
+
+            $("#mydiv2").css({"background-color":"yellowgreen",
+            "color":"red",
+            "font-size":"40px"
+            })
+        })
